cdad32f62785ac1d142b7409bd17e5c0bc5ab645
ripexz <ripexz@gmail.com>
2015-03-10 13:10:25 +0000

    Added prices

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var prices = {
+	raw: [
+		{"name": "Stone", "rate": 310},
+		{"name": "Iron Ore", "rate": 500},
+		{"name": "Nickel Ore", "rate": 20},
+		{"name": "Cobal Ore", "rate": 22},
+		{"name": "Magnesium Ore", "rate": 24},
+		{"name": "Silicon Ore", "rate": 22},
+		{"name": "Silver Ore", "rate": 20},
+		{"name": "Gold Ore", "rate": 20},
+		{"name": "Platinum Ore", "rate": 20},
+		{"name": "Uranium Ore", "rate": 44}
+	],
+	refined: [
+		{"name": "Gravel", "rate": 279},
+		{"name": "Iron Ingot", "rate": 350},
+		{"name": "Nickel Ingot", "rate": 8},
+		{"name": "Cobal Ingot", "rate": 6.6},
+		{"name": "Magnesium Powder", "rate": 0.168},
+		{"name": "Silicon Wafer", "rate": 15.4},
+		{"name": "Silver Ingot", "rate": 2.2},
+		{"name": "Gold Ingot", "rate": 0.2},
+		{"name": "Platinum Ingot", "rate": 0.1},
+		{"name": "Uranium Ingot", "rate": 0.308}
+	]
+}
